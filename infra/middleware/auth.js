12b4f003bd62839841fa95da92de6f60bdbc16b2
CARLOS ALBERTO SILVA JUNIOR <164648114+carlos-asj@users.noreply.github.com>
2025-12-10 19:11:12 +0000

    authentication, model users and middleware created

---
+++ infra/middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const { decode } = require("punycode");
+
+const authMiddleware = (req, res, next) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+
+  if (!token) {
+    return res.status(401).json({ error: "No token, authorization denied" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.indexOf;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Token is not valid" });
+  }
+};
+
+module.exports = authMiddleware;
--- infra/routes/auth.js
+++ infra/routes/auth.js
@@ -1,4 +1,61 @@
-const userExists = await User.findOne({ email });
-if (userExists) {
-  return res.status(400).json({ error: "User already exists" });
-}
+const express = require("espress");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const user = require("../models/users");
+const { error } = require("console");
+
+//register
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // user exists?
+    const userExists = await UserActivation.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+
+    //new user
+    const user = await user.create({ name, email, password });
+
+    // new token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
+
+    res.status(201).json({ token, user: { id: user._id, name, email } });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//login
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    //find user
+    const user = await user.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    //valid password
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    //token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
+
+    res.json({ token, user: { id: user._id, name: user.name, email } });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
