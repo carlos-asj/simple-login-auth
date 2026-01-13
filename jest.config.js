028f73337765342cc1d88997c8dce0094600026a
Carlos <darcilemons@gmail.com>
2026-01-13 15:50:59 -0300

    refactor the server archive

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-const jestConfig = {
-  moduleDirectories: ["node_modules", "<rootDir>"],
-  verbose: true,
-  transform: {
-    "\\.[jt]sx?$": "babel-jest",
-  },
-};
-
-module.exports = jestConfig;
